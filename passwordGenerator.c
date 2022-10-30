54dd9db66e6d65a59d7e4a008a9dfae599c79894
TharinduDakshina <tharindudakshina527@gmail.com>
2022-10-30 21:04:16 +0530

    password input compleat

--- passwordGenerator.c
+++ passwordGenerator.c
@@ -23,6 +23,8 @@ L1:	{
 		
 		if(checkHasNumber(length)==-1){
 			printf("\nYour password is invalid. Plece try again..\nPassword is will this type \"Hello4 or hello4\"");
+		}else{
+			printf("%d",11);
 		}
 			
 	}
@@ -57,51 +59,51 @@ int checkHasNumber(int length){
 	int count=-2;
 	for(i=0;i<length;i++){
 		switch(usertPassword[i]){
-			case 0:
+			case '0':
 				printf("%d",0);
-				count=1;
+				return 1;
 				break;
-			case 1:
+			case '1':
 				printf("%d",1);
-				count=1;
+				return 1;
 				break;
-			case 2:
+			case '2':
 				printf("%d",2);
-				count=1;
+				return 1;
 				break;
-			case 3:
+			case '3':
 				printf("%d",3);
-				count=1;
+				return 1;
 				break;
-			case 4:
+			case '4':
 				printf("%d",4);
-				count=1;
+				return 1;
 				break;
-			case 5:
-				printf("%d",5);
-				count=1;
+			case '5':
+				printf("\n%d\n",5);
+				return 1;
 				break;
-			case 6:
+			case '6':
 				printf("%d",6);
-				count=1;
+				return 1;
 				break;
-			case 7:
+			case '7':
 				printf("%d",7);
-				count=1;
+				return 1;
 				break;
-			case 8:
+			case '8':
 				printf("%d",8);
-				count=1;
-				break;
-			case 9:
+				return 1;
+				break; 
+			case '9':
 				printf("%d",9);
-				count=1;
+				return 1;
 				break;
 			default:
-				count=-1;
+				printf("\n\nRun default case");
 		}
 	}
-	return count;
+	
 }
 
 
